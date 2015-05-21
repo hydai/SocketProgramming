c3bdd79d63da705362fe250fdf5017b301bcfdd4
hydai <z54981220@gmail.com>
2015-05-21 13:19:25 +0800

    Update library

--- Homework2/hw2_lib.cpp
+++ Homework2/hw2_lib.cpp
@@ -1,6 +1,43 @@
-#include <iostream>
-#include <cstdlib>
 #include "hw2_lib.h"
+#include "const.h"
+
+int create_udp_server(struct sockaddr_in addr, int port) {
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    bzero(&addr, sizeof(addr));
+    addr.sin_family=AF_INET;
+    addr.sin_addr.s_addr=htonl(INADDR_ANY);
+    addr.sin_port=htons(PORT);
+    bind(sockfd, (struct sockaddr *)&addr, sizeof(addr));
+    return sockfd;
+}
+
+void server_echo(int sockfd) {
+    // Set socket timeout: http://stackoverflow.com/questions/13547721/udp-socket-set-timeout
+    char msg[MAXLINE+1];
+    struct sockaddr_in addr;
+    socklen_t len = sizeof(addr);
+    struct timeval tv;
+    tv.tv_sec = 1;
+    tv.tv_usec = 0;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
+        logging("setsocketopt failed");
+    }
+    while (true) {
+        int n = recvfrom(sockfd, msg, MAXLINE, 0, (struct sockaddr *)&addr, &len);
+        if (n < 0) {
+            // Skip garbage data
+            continue;
+        }
+        sendto(sockfd, msg, n, 0, (struct sockaddr *)&addr, len);
+    }
+}
+
+IP_INFO get_ip_info(struct sockaddr_in addr) {
+    IP_INFO ret;
+    ret.ip = inet_ntoa(addr.sin_addr);
+    ret.port = ntohs(addr.sin_port);
+    return ret;
+}
 
 void show_welcome_message() {
     system("clear");
@@ -18,6 +55,19 @@ void show_lobby_message(std::string &user_name) {
               << std::endl;
 }
 
+void show_article_list() {
+    system("clear");
+    std::cout << "**********Article List**********\n"
+              << "[E]Enter Article [D]Delete Article\n"
+              << std::endl;
+}
+
+void show_article_content() {
+    system("clear");
+    std::cout << "**********Article**********\n"
+              << "[U]Upload [D]Download [D]Delete Article\n"
+              << std::endl;
+}
 void logging(std::string msg) {
     time_t t = time(0);   // get time now
     struct tm * now = localtime( & t );
@@ -26,6 +76,28 @@ void logging(std::string msg) {
               << now->tm_sec
               << msg
               << std::endl;
+}
+
+string_vector string_split(std::string s) {
+    string_vector sp;
+    std::stringstream ss;
+    std::string tmp;
+    ss << s;
+    while (ss >> tmp) {
+        sp.push_back(tmp);
+    }
+    return sp;
+}
+
+std::string read_line() {
+    std::string ret;
+    std::getline(std::cin, ret);
+    return ret;
+}
+
+bool exec(std::string command) {
+    int status = system(command.c_str());
+    return (status == 0)?true:false;
 }
 
 // sqlite3
@@ -54,10 +126,10 @@ void close_db(sqlite3* &db) {
     sqlite3_close(db);
 }
 
-resultSet exec_sql(sqlite3* &db, std::string sql) {
+result_set exec_sql(sqlite3* &db, std::string sql) {
     char *errMsg = NULL;
     int rc = 0;
-    resultSet rs;
+    result_set rs;
     rs.clear();
     rc = sqlite3_exec(db, sql.c_str(), callback, (void *)&rs, &errMsg);
     if(rc != SQLITE_OK) {
@@ -70,7 +142,7 @@ resultSet exec_sql(sqlite3* &db, std::string sql) {
 }
 
 static int callback(void *vrs, int numOfCol, char **valueOfCol, char **nameOfCol){
-    resultSet rs = *(resultSet *)vrs;
+    result_set rs = *(result_set *)vrs;
     for (int i = 0; i < numOfCol; i++) {
         rs.insert( std::pair<std::string, std::string>(nameOfCol[i], (valueOfCol[i]?valueOfCol[i]:"NULL")) );
     }
--- Homework2/hw2_lib.h
+++ Homework2/hw2_lib.h
@@ -1,17 +1,65 @@
 #ifndef HW2_LIB_H
 #define HW2_LIB_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/signal.h>
+#include <sys/ioctl.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <dirent.h>
 #include <sqlite3.h>
+#include <iostream>
+#include <sstream>
 #include <map>
+#include <vector>
 #include <string>
-typedef std::map<std::string, std::string> resultSet;
+#include "const.h"
+
+// Type
+typedef std::map<std::string, std::string> result_set;
+typedef std::vector<std::string> string_vector;
+struct IP_INFO {
+    std::string ip;
+    int port;
+};
+
+// Execute external command
+bool exec(std::string command);
+
+// Socket
+IP_INFO get_ip_info(struct sockaddr_in addr);
+int create_udp_server(struct sockaddr_in addr, int port);
+void server_echo(int sockfd);
+
+// Message
 void show_welcome_message();
 void show_lobby_message();
+
+// String
 void logging(std::string msg);
+std::string read_line();
+string_vector string_split(std::string s);
+
+// Sqlite
 bool init_db(sqlite3* &db);
-resultSet exec_sql(sqlite3* &db, std::string sql);
+result_set exec_sql(sqlite3* &db, std::string sql);
 static int callback(void *vrs, int numOfCol, char **valueOfCol, char **nameOfCol);
 void close_db(sqlite3* &db);
 
+// Util
+template<typename T>
+T max(T a, T b) {
+    return (a>b)?a:b;
+}
+
+// Constents
 const int INIT_SQL_SIZE = 5;
 const int MAX_SQL_LENGTH = 500;
 const char init_sql[INIT_SQL_SIZE][MAX_SQL_LENGTH] = {
