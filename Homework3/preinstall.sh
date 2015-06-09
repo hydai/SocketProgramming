3f495d555aae8227cd0102b5df0f87a4dbf1851a
hydai <z54981220@gmail.com>
2015-06-09 11:17:54 +0800

    Remove broken Homework3

--- Homework3/preinstall.sh
+++
@@ -1,8 +0,0 @@
-#/bin/sh
-sudo apt-get update
-sudo apt-get install -y g++
-sudo apt-get install -y libsqlite3-dev sqlite3
-
-#Modify packet loss rate
-#sudo  tc qdisc add dev eth0 root netem loss 90%
-#sudo tc qdisc del dev eth0 root
